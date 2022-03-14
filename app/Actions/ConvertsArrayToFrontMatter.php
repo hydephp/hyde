3076ab14ea737c8cd359fb26f3168200b2ff8430
Caen De Silva <caen@desilva.se>
2022-03-14 14:10:34 +0100

    Create an array to front matter converter

---
+++ app/Actions/ConvertsArrayToFrontMatter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Actions;
+
+class ConvertsArrayToFrontMatter
+{
+    /**
+     * Convert an array into YAML Front Matter.
+     *
+     * @todo add support for nested arrays
+     *
+     * @param array $array
+     * @return string $yaml front matter
+     */
+    public function execute(array $array): string
+    {
+        $yaml = [];
+        $yaml[] = "---";
+        foreach ($array as $key => $value) {
+            $yaml[] = "$key: $value";
+        }
+
+        $yaml[] = '---';
+        $yaml[] = '';
+        return implode("\n", $yaml);
+    }
+}
