0babf63550fc65618b2110c41d902c792f8a880a
Caen De Silva <caen@desilva.se>
2022-03-14 20:31:55 +0100

    Create the Markdown Converter

---
+++ app/Hyde/Actions/MarkdownConverter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Hyde\Actions;
+
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+use Torchlight\Commonmark\V2\TorchlightExtension;
+
+/**
+ * Converts Markdown into HTML
+ */
+class MarkdownConverter
+{
+    /**
+     * Parse the Markdown into HTML.
+     *
+     * @param string $markdown
+     * @return string $html
+     */
+    public static function parse(string $markdown): string
+    {
+        $converter = new CommonMarkConverter();
+
+        $converter->getEnvironment()->addExtension(new GithubFlavoredMarkdownExtension());
+
+        return  $converter->convert($markdown);
+    }
+}
