3266a7df8f422a8c865f88569553e5e66c39b308
Caen De Silva <caen@desilva.se>
2022-03-15 17:34:21 +0100

    Switch to https for schema definition

--- resources/views/post/author.blade.php
+++ resources/views/post/author.blade.php
@@ -1,4 +1,4 @@
-<address itemprop="author publisher" itemscope itemtype="http://schema.org/Person"> 
+<address itemprop="author publisher" itemscope itemtype="https://schema.org/Person"> 
 	By <span itemprop="name">{{ $post->matter['author'] }}</span> 
 	{{-- @todo Add more properties such as social media links --}}
 </address> 
