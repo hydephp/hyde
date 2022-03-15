098ffe0ee51ba7c7772f5a236424742780b9562f
Caen De Silva <caen@desilva.se>
2022-03-15 14:53:52 +0100

    Create the Hyde config file

---
+++ config/hyde.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+|      __ __        __    ___  __ _____ 
+|     / // /_ _____/ /__ / _ \/ // / _ \
+|    / _  / // / _  / -_) ___/ _  / ___/
+|   /_//_/\_, /\_,_/\__/_/  /_//_/_/    
+|        /___/                          
+|--------------------------------------------------------------------------
+|
+| Welcome to HydePHP! In this file you can customize your new Static Site!
+|
+| HydePHP favors convention over configuration and as such requires virtually
+| no configuration out of the box to get started. Though, you may want to
+| change the options to personalize your site and make it your own!
+|
+*/
+return [
+
+	/*
+    |--------------------------------------------------------------------------
+    | Site Name
+    |--------------------------------------------------------------------------
+    |
+    | This value sets the name of your site and is, for example, used in
+    | the compiled page titles and more. Default value is "HydePHP".
+    |
+    */
+
+    'name' => 'HydePHP',
+
+];
