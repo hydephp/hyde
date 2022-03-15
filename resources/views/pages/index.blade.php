603cbd79f6c990c596c7ddb107398f24fc54ce79
Caen De Silva <caen@desilva.se>
2022-03-15 16:56:41 +0100

    Add the latest posts

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,26 @@
 @extends('layouts.app')
 @section('content')
 
-// Place content here
+    <h1>{{ config('hyde.name') }}</h1>
+
+    <section>
+        <h2>Latest Posts</h2>
+        
+        @foreach(\App\Hyde\Models\MarkdownPost::getCollection() as $post)
+        <article itemscope itemtype="https://schema.org/Article">
+            <header>
+                <h3 itemprop="headline">{{ $post->matter['title'] }}</h3>
+                @includeWhen(isset($post->matter['date']), 'post.datePublished')
+                @includeWhen(isset($post->matter['author']), 'post.author')
+                @includeWhen(isset($post->matter['category']), 'post.category')
+            </header>
+            @includeWhen(isset($post->matter['description']), 'post.description')
+            <footer>
+                <a href="posts/{{ $post->matter['slug'] }}.html" itemprop="url">Read Post</a>
+            </footer>
+        </article>
+        @endforeach
+
+    </section>
 
 @endsection
