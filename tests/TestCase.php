83f03913bfee11498c5112159b89a71b5e0f8f8f
Caen De Silva <caen@desilva.se>
2022-03-15 12:36:55 +0100

    Add the set up and tear down methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,22 @@ use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Setup the test environment.
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
 }
