3fc8e038d5a335b27786e5bc9ff3e1b704b391fe
Caen De Silva <caen@desilva.se>
2022-03-16 11:46:54 +0100

    Fix bug where front matter title was not showing

--- resources/views/post/article.blade.php
+++ resources/views/post/article.blade.php
@@ -1,6 +1,6 @@
 <article id="post-article" itemscope itemtype="http://schema.org/Article">
     <header>
-        <h1 itemprop="headline">{{ $title ?? 'Blog Post' }}</h1>
+        <h1 itemprop="headline">{{ $post->matter['title'] ?? 'Blog Post' }}</h1>
 		<div id="byline">
             @includeWhen(isset($post->matter['date']), 'post.datePublished')
 		    @includeWhen(isset($post->matter['author']), 'post.author')
