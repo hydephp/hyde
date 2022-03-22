88f9e8e506766e22b4db754a440a68b7ba7dd22f
Caen De Silva <caen@desilva.se>
2022-03-22 14:42:40 +0100

    Only run test if custom Blade pages exist

--- tests/Validators/CheckForPageConflictsTest.php
+++ tests/Validators/CheckForPageConflictsTest.php
@@ -1,23 +1,25 @@
 <?php
 
-test('check for conflicts between blade and markdown pages', function () {
-    $markdownPages = [];
-    $bladePages = [];
+if (file_exists('resources/views/pages')) {
+    test('check for conflicts between blade and markdown pages', function () {
+            $markdownPages = [];
+        $bladePages = [];
 
-    foreach (array_diff(scandir('_pages'), array('..', '.')) as $page) {
-        $markdownPages[] = basename($page, '.md');
-    }
+        foreach (array_diff(scandir('_pages'), array('..', '.')) as $page) {
+            $markdownPages[] = basename($page, '.md');
+        }
 
-    foreach (array_diff(scandir('resources/views/pages'), array('..', '.')) as $page) {
-        $bladePages[] = basename($page, '.blade.php');
-    }
+            foreach (array_diff(scandir('resources/views/pages'), array('..', '.')) as $page) {
+                $bladePages[] = basename($page, '.blade.php');
+            }
 
-    $conflicts = array_intersect($markdownPages, $bladePages);
+        $conflicts = array_intersect($markdownPages, $bladePages);
+        
+        if (count($conflicts)) {
+            $this->addWarning('Found conflicts: ' . implode(', ', $conflicts)); 
+        } else {
+            expect($conflicts)->toBeEmpty();
+        }
     
-    if (count($conflicts)) {
-        $this->addWarning('Found conflicts: ' . implode(', ', $conflicts)); 
-    } else {
-        expect($conflicts)->toBeEmpty();
-    }
-   
-})->group('validators');
+    })->group('validators');
+}
