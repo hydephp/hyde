8c0d33158941579cf309847e865c8a0dda3772ad
Caen De Silva <caen@desilva.se>
2022-03-23 16:39:38 +0100

    Update config

--- config/hyde.php
+++ config/hyde.php
@@ -50,6 +50,30 @@ return [
     */
     'site_url' => env('SITE_URL', null),
 
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Global Site Meta Tags
+    |--------------------------------------------------------------------------
+    |
+    | While you can add any number of meta tags in the meta.blade.php component,
+    | this config setting allows you to easily customize some common metadata
+    | tags so increase your SEO score. You can keep them to their defaults
+    | or you can set a value to false or null to disable it completely.
+    |
+    | Note that some tags may be overwritten on certain pages, for instance post pages.
+    |
+    | Tip: See https://www.w3schools.com/tags/att_meta_name.asp
+    | for a list of tags and what what they are for.
+    |
+    */
+    'meta' => [
+        // 'author' => 'Mr. Hyde',
+        // 'description' => 'My Hyde Blog',
+        // 'keywords' => 'Static Sites, Blogs, Documentation',
+        'generator' => 'HydePHP ' . app('git.version'),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Features
