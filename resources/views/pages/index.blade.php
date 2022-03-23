b3f639283fe047f14b493765393e0a382f4c93ef
Caen De Silva <caen@desilva.se>
2022-03-23 22:11:29 +0100

    Update the index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -39,12 +39,19 @@
                     <span class="block text-4xl md:text-5xl mb-4 sm:mb-0">You're running on</span> <span
                         class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br logo-gradient md:inline-block drop-shadow-2xl tracking-normal">HydePHP</span>
                 </h1>
-                <div class="mx-auto mt-8 sm:mt-4 text-gray-200 md:mt-8 md:max-w-2xl md:text-center lg:text-lg">
-                    <p aria-label="About Hyde">
+                <div class="mx-auto mt-8 sm:mt-4 text-gray-200 md:mt-8 md:max-w-2xl md:text-center">
+                    <p aria-label="About Hyde" class="lg:text-lg">
                         Leap into the future of static HTML blogs and documentation with the tools you already know and
                         love. Made with Tailwind, Laravel, and Coffee.
                     </p>
-                    <p aria-label="Getting Started" class="mt-4 text-white flex flex-wrap justify-center">
+                    <p aria-label="About this page" class="mt-4 mb-4">
+                        This is the default homepage stored as index.blade.php, however you can publish any of the built in views using the following command:
+                                                
+                        <!-- Syntax highlighted by torchlight.dev -->
+                        <pre style="margin-top: 1.5rem; margin-bottom: 1.5rem;"><code data-theme="material-theme-palenight" data-lang="bash" class="torchlight" style="background-color: #292D3E; padding: 0.5rem 1rem; border-radius: 0.25rem;"><span style="color: #FFCB6B;">php hyde</span> <span style="color: #C3E88D;">publish:homepage</span></code></pre>
+                        
+                    </p>
+                    <p aria-label="Getting started" class="mt-4 md:mt-8 text-white flex flex-wrap justify-center">
                         <a href="https://hydephp.github.io/docs/docs" class="uppercase font-bold text-sm flex text-center m-2 mx-3">
                             Documentation
                         </a>
