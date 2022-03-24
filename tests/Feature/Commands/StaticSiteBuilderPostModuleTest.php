bca234c40ba35f3d90c4a4cb756d75070938ec9d
Caen De Silva <caen@desilva.se>
2022-03-24 21:19:30 +0100

    Inline the stream variable to fix missing file error

--- tests/Feature/Commands/StaticSiteBuilderPostModuleTest.php
+++ tests/Feature/Commands/StaticSiteBuilderPostModuleTest.php
@@ -24,20 +24,12 @@ class StaticSiteBuilderPostModuleTest extends TestCase
      * @var string
      */
     protected string $file;
-
-    /**
-     * The contents of the file
-     *
-     * @var string|null
-     */
-    protected string|null $stream = null;
-
+ 
     public function __construct()
     {
         parent::__construct();
 
         $this->file = Hyde::path('_site/posts/alice-in-wonderland.html');
-        $this->stream = file_get_contents($this->file);
     }
     
     public function test_setup()
@@ -57,26 +49,21 @@ class StaticSiteBuilderPostModuleTest extends TestCase
     {
         $this->assertGreaterThan(1024, filesize($this->file), 'Failed asserting that file is larger than one kilobyte');
         
-        $this->assertStringContainsStringIgnoringCase('<!DOCTYPE html>', $this->stream);
-        $this->assertStringContainsString('HydePHP', $this->stream);
-        $this->assertStringContainsString('tailwind', $this->stream);
+        $stream = file_get_contents($this->file);
+        $this->assertStringContainsStringIgnoringCase('<!DOCTYPE html>', $stream);
+        $this->assertStringContainsString('HydePHP', $stream);
+        $this->assertStringContainsString('tailwind', $stream);
     }
 
 
     public function test_created_post_contains_expected_content()
     {
-        $this->assertStringContainsString('Alice&#039;s Adventures in Wonderland', $this->stream);
-        $this->assertStringContainsString('Lewis Carroll', $this->stream);
-        $this->assertStringContainsString('<h2>CHAPTER I. DOWN THE RABBIT-HOLE.</h2>', $this->stream);
-        $this->assertStringContainsString('So she was considering in her own mind, (as well as she could, for the hot day made her feel very sleepy and stupid,) whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a white rabbit with pink eyes ran close by her.', $this->stream);
-        $this->assertStringContainsString('<p><img src="https://upload.wikimedia.org/wikipedia/commons/6/63/Alice_par_John_Tenniel_04.png" alt="Illustration d\'origine (1865), par John Tenniel" /></p>', $this->stream);
+        $stream = file_get_contents($this->file);
+        $this->assertStringContainsString('Alice&#039;s Adventures in Wonderland', $stream);
+        $this->assertStringContainsString('Lewis Carroll', $stream);
+        $this->assertStringContainsString('<h2>CHAPTER I. DOWN THE RABBIT-HOLE.</h2>', $stream);
+        $this->assertStringContainsString('So she was considering in her own mind, (as well as she could, for the hot day made her feel very sleepy and stupid,) whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a white rabbit with pink eyes ran close by her.', $stream);
+        $this->assertStringContainsString('<p><img src="https://upload.wikimedia.org/wikipedia/commons/6/63/Alice_par_John_Tenniel_04.png" alt="Illustration d\'origine (1865), par John Tenniel" /></p>', $stream);
     }
 
-    public function test_teardown()
-    {
-        unset($this->file);
-        unset($this->stream);
-
-        $this->assertTrue(true);
-    }
 }
