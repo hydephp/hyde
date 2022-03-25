f4f1943e006de344c68ce625f57cf32631c5315e
Caen De Silva <caen@desilva.se>
2022-03-25 18:10:15 +0100

    Add the test

---
+++ tests/Feature/Commands/ValidateCommandTest.php
@@ -0,0 +1,12 @@
+<?php
+
+test('validate command can run', function () {
+    $this->artisan('validate')->assertExitCode(0);
+});
+
+it('prints debug information', function () {
+    $this->artisan('validate')
+        ->expectsOutput('Running validation tests!')
+		->expectsOutput('All done!')
+        ->assertExitCode(0);
+});
