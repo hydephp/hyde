9dbfdcaa2198ed151beb5fd0e8d51479a2666d17
Caen De Silva <caen@desilva.se>
2022-03-26 19:39:20 +0100

    Check the variable from the config instead of app

--- tests/Setup/EnsureAppEnvIsTestingSmokeTest.php
+++ tests/Setup/EnsureAppEnvIsTestingSmokeTest.php
@@ -8,6 +8,6 @@ class EnsureAppEnvIsTestingSmokeTest extends TestCase
 {
     public function test_app_env_is_testing()
     {
-        $this->assertEquals('testing', app('env'));
+        $this->assertEquals('testing', config('app.env'));
     }
 }
