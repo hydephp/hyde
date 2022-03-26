19ae89e1927b89df356a27a5ddb500f97b959892
Caen De Silva <caen@desilva.se>
2022-03-26 19:24:18 +0100

    Run a smoketest for github actions

---
+++ tests/Unit/Smoketest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Smoketest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_if_tests_are_working()
+    {
+        $this->assertTrue(false);
+    }
+}
