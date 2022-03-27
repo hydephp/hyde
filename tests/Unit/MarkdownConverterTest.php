188e838932c0305ec7eaf54c0f2eb3c89c4c0681
Caen De Silva <caen@desilva.se>
2022-03-27 19:18:48 +0200

    Add the test

---
+++ tests/Unit/MarkdownConverterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Hyde\Framework\Actions\MarkdownConverter;
+use Tests\TestCase;
+
+/**
+ * Class MarkdownConverterTest.
+ *
+ * @covers \Hyde\Framework\Actions\MarkdownConverter
+ */
+class MarkdownConverterTest extends TestCase
+{
+    public function testParse(): void
+    {
+		$markdown = "# Hello World!";
+
+        $html = MarkdownConverter::parse($markdown);
+
+		$this->assertIsString($html);
+		$this->assertEquals("<h1>Hello World!</h1>\n", $html);
+    }
+}
