f5bfc14ead00af41d5007473cdf193df7e6ace13
Caen De Silva <caen@desilva.se>
2022-03-28 13:03:01 +0200

    Rename test file

--- tests/Setup/RunDebugCommandTest.php
+++ tests/Setup/RunDebugCommandTest.php
@@ -4,7 +4,7 @@ namespace Tests\Setup;
 
 use PHPUnit\Framework\TestCase;
 
-class DebugCommandTest extends TestCase
+class RunDebugCommandTest extends TestCase
 {
     public function test_print_debug_info()
     {
