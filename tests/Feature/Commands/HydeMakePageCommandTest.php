f445bd57aff02d1619320ecd5dcbea9a09112c68
Caen De Silva <caen@desilva.se>
2022-04-01 20:50:13 +0200

    Create the test

---
+++ tests/Feature/Commands/HydeMakePageCommandTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Hyde\Framework\Commands;
+
+use Exception;
+use Hyde\Framework\Hyde;
+use Tests\TestCase;
+
+class HydeMakePageCommandTest extends TestCase
+{
+	protected string $markdownPath;
+	protected string $bladePath;
+
+	public function __construct() {
+        parent::__construct();
+
+		$this->markdownPath = Hyde::path('_pages/8450de2-test-page.md');
+		$this->bladePath = Hyde::path('resources/views/pages/8450de2-test-page.blade.php');
+	}
+
+	protected function tearDown(): void
+	{
+		if (file_exists($this->markdownPath)) {
+			unlink($this->markdownPath);
+		}
+
+		if (file_exists($this->bladePath)) {
+			unlink($this->bladePath);
+		}
+
+		parent::tearDown();
+	}
+
+	// Assert the command can run
+	public function test_command_can_run()
+	{
+		$this->artisan('make:page "8450de2 test page"')->assertExitCode(0);
+	}
+
+	// Assert the command contains expected output
+	public function test_command_output()
+	{
+		$this->artisan('make:page "8450de2 test page"')
+			->expectsOutputToContain('Creating a new page!')
+			->expectsOutputToContain('Created file ' . $this->markdownPath);
+	}
+
+	// Assert the command allows the user to specify a page type
+	public function test_command_allows_user_to_specify_page_type()
+	{
+		$this->artisan('make:page "8450de2 test page" --type=markdown')->assertExitCode(0);
+		$this->artisan('make:page "8450de2 test page" --type=blade')->assertExitCode(0);
+	}
+
+	// Assert the type option is case-insensitive
+	public function test_type_option_is_case_insensitive()
+	{
+		$this->artisan('make:page "8450de2 test page" --type=Markdown')->assertExitCode(0);
+		$this->artisan('make:page "8450de2 test page" --type=Blade')->assertExitCode(0);
+	}
+
+	// Assert that the command fails if the user specifies an invalid page type
+	public function test_command_fails_if_user_specifies_invalid_page_type()
+	{
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('Invalid page type: invalid');
+		$this->expectExceptionCode(400);
+		$this->artisan('make:page "8450de2 test page" --type=invalid')->assertExitCode(400);
+	}
+
+	// Assert the command creates the markdown file
+	public function test_command_creates_markdown_file()
+	{
+		$this->artisan('make:page "8450de2 test page"')->assertExitCode(0);
+
+		$this->assertFileExists($this->markdownPath);
+	}
+
+	// Assert the command creates the blade file
+	public function test_command_creates_blade_file()
+	{
+		$this->artisan('make:page "8450de2 test page" --type="blade"')->assertExitCode(0);
+
+		$this->assertFileExists($this->bladePath);
+	}
+}
