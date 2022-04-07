26758975dccf84a8d7b072a76df7a78982bc5dea
Caen De Silva <caen@desilva.se>
2022-04-07 21:09:20 +0200

    Create the validator

--- tests/Validators/CheckThatFrontendAssetsExistTest.php
+++ tests/Validators/CheckThatFrontendAssetsExistTest.php
@@ -1,17 +1,26 @@
 <?php
 
+test('check that hyde.css exists', function () {
+    if (! file_exists('_site/media/hyde.css')) {
+        $this->addWarning('Could not find the `Hyde.css` stylesheet in the build directory. You may need to run `php hyde update:resources`.');
+    } else {
+        $this->assertTrue(true);
+    }
+})->group('validators');
+
+test('check that hyde.js exists', function () {
+    if (! file_exists('_site/media/hyde.js')) {
+        $this->addWarning('Could not find the `Hyde.js` stylesheet in the build directory. You may need to run `php hyde update:resources`.');
+    } else {
+        $this->assertTrue(true);
+    }
+})->group('validators');
+
 test('check that app.css exist', function () {
     if (! file_exists('_site/media/app.css')) {
         $this->addWarning('Could not find the app stylesheet in the build directory. You may need to run `npm run dev`.');
     } else {
-        $this->assertTrue(file_exists('_site/media/app.css'));
+        $this->assertTrue(true);
     }
 })->group('validators');
 
-test('check that tailwind.css exist', function () {
-    if (! file_exists('_site/media/tailwind.css')) {
-        $this->addWarning('Could not find the tailwind stylesheet in the build directory. You may need to run `npm run dev`.');
-    } else {
-        $this->assertTrue(file_exists('_site/media/tailwind.css'));
-    }
-})->group('validators');
