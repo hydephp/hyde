f1e7d8a6d356aacc62b83f6638f1d721ecfe8aa7
Caen De Silva <caen@desilva.se>
2022-04-07 20:05:57 +0200

    Add the test

---
+++ tests/Feature/Commands/HydePublishFrontendResourcesCommandTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Commands;
+
+use Hyde\Framework\Hyde;
+use Illuminate\Support\Facades\File;
+use App\Commands\TestWithBackup;
+use Tests\TestCase;
+
+class HydePublishFrontendResourcesCommandTest extends TestCase
+{
+	/** Setup */
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		TestWithBackup::backupDirectory(Hyde::path('resources/frontend'));
+		File::deleteDirectory(Hyde::path('resources/frontend'));
+	}
+
+	/** @test */
+    public function testCommandReturnsZeroExitCode()
+    {
+        $this->artisan('update:resources --force')
+            ->doesntExpectOutput('Please note that the following files will be overwritten:')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function testCommandHasExpectedOutput()
+    {
+        $this->artisan('update:resources ')
+            ->expectsOutput('Publishing frontend resources!')
+            ->expectsConfirmation('Would you like to continue?', false)
+            ->expectsOutput('Okay. Aborting.')
+            ->assertExitCode(1);
+    }
+
+    /** @test */
+    public function testCommandHasCanPublishResources()
+    {
+        $this->artisan('update:resources')
+            ->expectsConfirmation('Would you like to continue?', 'yes')
+            ->expectsOutput('Okay. Proceeding.')
+            ->assertExitCode(0);
+
+        $this->assertDirectoryExists(Hyde::path('resources/frontend'));
+    }
+
+	/** Teardown */
+	public function tearDown(): void
+	{
+		TestWithBackup::restoreDirectory(Hyde::path('resources/frontend'));
+
+		parent::tearDown();
+	}
+}
