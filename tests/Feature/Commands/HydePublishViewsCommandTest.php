fd4f511082a91bc766a8607d8400c75837edea65
Caen De Silva <caen@desilva.se>
2022-04-10 19:38:12 +0200

    Clean up after test and add assertion that files are actually published

--- tests/Feature/Commands/HydePublishViewsCommandTest.php
+++ tests/Feature/Commands/HydePublishViewsCommandTest.php
@@ -2,15 +2,35 @@
 
 namespace Tests\Feature\Commands;
 
+use App\Commands\TestWithBackup;
+use Hyde\Framework\Hyde;
+use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
 class HydePublishViewsCommandTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        TestWithBackup::backupDirectory(Hyde::path('resources/views/vendor/hyde'));
+        File::deleteDirectory(Hyde::path('resources/views/vendor/hyde'));
+    }
+
+    protected function tearDown(): void
+    {
+        TestWithBackup::restoreDirectory(Hyde::path('resources/views/vendor/hyde'));
+
+        parent::tearDown();
+    }
+
     public function test_command_publishes_views()
     {
         $this->artisan('publish:views --all')
             ->expectsOutput('Publishing complete.')
             ->assertExitCode(0);
+
+        $this->assertFileExists(Hyde::path('resources/views/vendor/hyde/layouts/app.blade.php'));
     }
 
     public function test_command_prompts_for_input()
