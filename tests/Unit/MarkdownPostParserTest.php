39e544d61fef68c704ec9904e8f1e152e7cbf498
Caen De Silva <caen@desilva.se>
2022-04-10 21:10:42 +0200

    Refactor to be self-contained

--- tests/Unit/MarkdownPostParserTest.php
+++ tests/Unit/MarkdownPostParserTest.php
@@ -12,45 +12,23 @@ use PHPUnit\Framework\TestCase;
  */
 class MarkdownPostParserTest extends TestCase
 {
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
     protected function setUp(): void
     {
         parent::setUp();
 
-        // Create a Markdown file to work with
-        copy(Hyde::path('vendor/hyde/framework/tests/stubs/_posts/test-parser-post.md'), $this->getPath());
+        createTestPost();
     }
 
-    /**
-     * Clean up the testing environment before the next test.
-     *
-     * @return void
-     */
     protected function tearDown(): void
     {
-        // Remove the published stub file
-        unlink($this->getPath());
+        unlink(Hyde::path('_posts/test-post.md'));
 
         parent::tearDown();
     }
 
-    /**
-     * Get the path of the test Markdown file.
-     *
-     * @return string
-     */
-    public function getPath(): string
-    {
-        return Hyde::path('_posts/test-parser-post.md');
-    }
-
     public function test_can_parse_markdown_file()
     {
-        $post = (new MarkdownPostParser('test-parser-post'))->get();
+        $post = (new MarkdownPostParser('test-post'))->get();
         $this->assertInstanceOf(MarkdownPost::class, $post);
         $this->assertCount(4, ($post->matter));
         $this->assertIsArray($post->matter);
@@ -62,10 +40,10 @@ class MarkdownPostParserTest extends TestCase
 
     public function test_parsed_markdown_post_contains_valid_front_matter()
     {
-        $post = (new MarkdownPostParser('test-parser-post'))->get();
+        $post = (new MarkdownPostParser('test-post'))->get();
         $this->assertEquals('My New Post', $post->matter['title']);
         $this->assertEquals('Mr. Hyde', $post->matter['author']);
         $this->assertEquals('blog', $post->matter['category']);
-        $this->assertEquals('test-parser-post', $post->matter['slug']);
+        $this->assertEquals('test-post', $post->matter['slug']);
     }
 }
