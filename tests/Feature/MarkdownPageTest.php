de02c71289f11072c6ed356de95d1ee565ae7f14
Caen De Silva <caen@desilva.se>
2022-04-10 21:35:55 +0200

    Restore the environment after testing

--- tests/Feature/MarkdownPageTest.php
+++ tests/Feature/MarkdownPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Commands\TestWithBackup;
 use Exception;
 use Hyde\Framework\Hyde;
 use Hyde\Framework\MarkdownPageParser;
@@ -10,22 +11,37 @@ use Hyde\Framework\Services\CollectionService;
 use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
+/**
+ * Test the Markdown page parser.
+ */
 class MarkdownPageTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        TestWithBackup::backupDirectory(Hyde::path('_pages'));
+
+        file_put_contents(Hyde::path('_pages/test-post.md'), "# PHPUnit Test File \n Hello World!");
+    }
+
+    protected function tearDown(): void
+    {
+        TestWithBackup::restoreDirectory(Hyde::path('_pages'));
+
+        parent::tearDown();
+    }
+
     /**
      * Test the Parser.
      */
     public function test_can_get_collection_of_slugs()
     {
-        File::deleteDirectory(Hyde::path('_pages'));
-        mkdir(Hyde::path('_pages'));
-        file_put_contents(Hyde::path('_pages/phpunit-test.md'), "# PHPUnit Test File \n Hello World!");
-
         $array = CollectionService::getMarkdownPageList();
 
         $this->assertIsArray($array);
         $this->assertCount(1, $array);
-        $this->assertArrayHasKey('phpunit-test', array_flip($array));
+        $this->assertArrayHasKey('test-post', array_flip($array));
     }
 
     public function test_exception_is_thrown_for_missing_slug()
@@ -37,20 +53,20 @@ class MarkdownPageTest extends TestCase
 
     public function test_can_parse_documentation_page()
     {
-        $parser = new MarkdownPageParser('phpunit-test');
+        $parser = new MarkdownPageParser('test-post');
         $this->assertInstanceOf(MarkdownPageParser::class, $parser);
     }
 
     public function test_title_was_inferred_from_heading()
     {
-        $parser = new MarkdownPageParser('phpunit-test');
+        $parser = new MarkdownPageParser('test-post');
         $this->assertIsString($parser->title);
         $this->assertEquals('PHPUnit Test File', $parser->title);
     }
 
     public function test_parser_contains_body_text()
     {
-        $parser = new MarkdownPageParser('phpunit-test');
+        $parser = new MarkdownPageParser('test-post');
         $this->assertIsString($parser->body);
         $this->assertEquals("# PHPUnit Test File \n Hello World!", $parser->body);
     }
@@ -60,7 +76,7 @@ class MarkdownPageTest extends TestCase
      */
     public function test_can_get_page_model_object(): MarkdownPage
     {
-        $parser = new MarkdownPageParser('phpunit-test');
+        $parser = new MarkdownPageParser('test-post');
         $object = $parser->get();
         $this->assertInstanceOf(MarkdownPage::class, $object);
 
@@ -74,6 +90,6 @@ class MarkdownPageTest extends TestCase
     {
         $this->assertEquals('PHPUnit Test File', $object->title);
         $this->assertEquals("# PHPUnit Test File \n Hello World!", $object->body);
-        $this->assertEquals('phpunit-test', $object->slug);
+        $this->assertEquals('test-post', $object->slug);
     }
 }
