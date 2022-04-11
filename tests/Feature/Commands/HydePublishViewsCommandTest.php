7490a47af855aadf46858348503625dd8e126f56
Caen De Silva <caen@desilva.se>
2022-04-11 22:00:15 +0200

    Update the test for refactored command

--- tests/Feature/Commands/HydePublishViewsCommandTest.php
+++ tests/Feature/Commands/HydePublishViewsCommandTest.php
@@ -24,8 +24,8 @@ class HydePublishViewsCommandTest extends TestCase
 
     public function test_command_publishes_views()
     {
-        $this->artisan('publish:views --all')
-            ->expectsOutput('Publishing complete.')
+        $this->artisan('publish:views all')
+            ->expectsOutput('Copied [vendor/hyde/framework/resources/views/pages/404.blade.php] to [_pages/404.blade.php]')
             ->assertExitCode(0);
 
         $this->assertFileExists(Hyde::path('resources/views/vendor/hyde/layouts/app.blade.php'));
@@ -34,7 +34,7 @@ class HydePublishViewsCommandTest extends TestCase
     public function test_command_prompts_for_input()
     {
         $this->artisan('publish:views')
-            ->expectsQuestion('Which view categories (tags) would you like to publish?', 'Tag: hyde-components')
+            ->expectsQuestion('Which category do you want to publish?', 'all')
             ->assertExitCode(0);
     }
 }
