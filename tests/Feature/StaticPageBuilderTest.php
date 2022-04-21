2bba5982872cc20dc8a9da149c261be99b358d16
Caen De Silva <caen@desilva.se>
2022-04-21 19:57:41 +0200

    Implement the test

--- tests/Feature/StaticPageBuilderTest.php
+++ tests/Feature/StaticPageBuilderTest.php
@@ -2,7 +2,15 @@
 
 namespace Tests\Feature;
 
+use Hyde\Framework\Actions\CreatesDefaultDirectories;
+use Hyde\Framework\Hyde;
+use Hyde\Framework\Models\BladePage;
+use Hyde\Framework\Models\DocumentationPage;
+use Hyde\Framework\Models\MarkdownPage;
+use Hyde\Framework\Models\MarkdownPost;
 use Hyde\Framework\StaticPageBuilder;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
 /**
@@ -10,9 +18,9 @@ use Tests\TestCase;
  *
  * Feature tests for the StaticPageBuilder class.
  *
- * @see \Hyde\Framework\StaticPageBuilder
+ * @covers \Hyde\Framework\StaticPageBuilder
  *
- * The compiled HTML is tested in a separate unit test.
+ * The compiled HTML content is tested in a separate unit test.
  * @see \Tests\Unit\ViewCompilerTest
  */
 class StaticPageBuilderTest extends TestCase
@@ -22,56 +30,75 @@ class StaticPageBuilderTest extends TestCase
         parent::setUp();
 
         // Back up any existing files
+        backupDirectory(Hyde::path('_site'));
+
+        // Clean the site directory
+        File::deleteDirectory(Hyde::path('_site'));
+    
+        // Recreate the default directories
+        (new CreatesDefaultDirectories)->__invoke();
     }
 
     protected function tearDown(): void
     {
         // Restore the file environment and any backed up files
+        restoreDirectory(Hyde::path('_site'));
 
         parent::tearDown();
     }
 
+    public function test_can_build_blade_page()
+    {
+        file_put_contents(BladePage::$sourceDirectory . '/foo.blade.php', 'bar');
+
+        $page = new BladePage('foo');
+
+        new StaticPageBuilder($page, true);
+
+        $this->assertFileExists(Hyde::path('_site/foo.html'));
+
+        unlink(BladePage::$sourceDirectory . '/foo.blade.php');
+    }
+
+
     public function test_can_build_markdown_post()
     {
-        $this->markTestIncomplete();
+        $page = new MarkdownPost([
+            'title' => 'foo',
+            'author' => 'bar',
+        ], '# Body', 'Title', 'foo');
+        
+        new StaticPageBuilder($page, true);
 
-        // Run the builder
-
-        // Make the assertions
-
-        // Clean up after the test
+        $this->assertFileExists(Hyde::path('_site/posts/foo.html'));
     }
 
     public function test_can_build_markdown_page()
     {
-        $this->markTestIncomplete();
+        $page = new MarkdownPage([], '# Body', 'Title', 'foo');
 
-        // Run the builder
+        new StaticPageBuilder($page, true);
 
-        // Make the assertions
-
-        // Clean up after the test
-    }
-
-    public function test_can_build_blade_page()
-    {
-        $this->markTestIncomplete();
-
-        // Run the builder
-
-        // Make the assertions
-
-        // Clean up after the test
+        $this->assertFileExists(Hyde::path('_site/foo.html'));
     }
 
     public function test_can_build_documentation_page()
     {
-        $this->markTestIncomplete();
+        $page = new DocumentationPage([], '# Body', 'Title', 'foo');
 
-        // Run the builder
+        new StaticPageBuilder($page, true);
 
-        // Make the assertions
+        $this->assertFileExists(Hyde::path('_site/' . Hyde::docsDirectory() . '/foo.html'));
+    }
 
-        // Clean up after the test
+    public function test_creates_custom_documentation_directory()
+    {
+        $page = new DocumentationPage([], '# Body', 'Title', 'foo');
+
+        Config::set('hyde.docsDirectory', 'docs/foo');
+
+        new StaticPageBuilder($page, true);
+
+        $this->assertFileExists(Hyde::path('_site/docs/foo/foo.html'));
     }
 }
