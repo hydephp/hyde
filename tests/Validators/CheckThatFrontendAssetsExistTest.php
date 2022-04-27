fa68c3718b1454663089e3a53791b6996f5d1a1d
Caen De Silva <caen@desilva.se>
2022-04-27 20:03:17 +0200

    Update tests for removed frontend assets

--- tests/Validators/CheckThatFrontendAssetsExistTest.php
+++ tests/Validators/CheckThatFrontendAssetsExistTest.php
@@ -1,21 +1,5 @@
 <?php
 
-test('check that hyde.css exists', function () {
-    if (! file_exists('_site/media/hyde.css')) {
-        $this->addWarning('Could not find the `Hyde.css` stylesheet in the build directory. You may need to run `php hyde update:assets`.');
-    } else {
-        $this->assertTrue(true);
-    }
-})->group('validators');
-
-test('check that hyde.js exists', function () {
-    if (! file_exists('_site/media/hyde.js')) {
-        $this->addWarning('Could not find the `Hyde.js` stylesheet in the build directory. You may need to run `php hyde update:assets`.');
-    } else {
-        $this->assertTrue(true);
-    }
-})->group('validators');
-
 test('check that app.css exist', function () {
     if (! file_exists('_site/media/app.css')) {
         $this->addWarning('Could not find the app stylesheet in the build directory. You may need to run `npm run dev`.');
