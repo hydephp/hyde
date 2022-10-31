98a9e54823baf8f28fc7e983dcf12f12033033dd
Caen De Silva <caen@desilva.se>
2022-10-31 14:20:07 +0100

    Revert "Update StaticSiteBuilderTest.php https://github.com/hydephp/develop/commit/6d6cad42f6f81736c0a7ee86947aac788e55f93c"
    This reverts commit 3473452496e9db9b738fda3d8881a8d49879becc.

--- config/hyde.php
+++ config/hyde.php
@@ -11,9 +11,9 @@
 |
 */
 
-use Hyde\Facades\Author;
-use Hyde\Facades\Features;
-use Hyde\Facades\Meta;
+use Hyde\Framework\Helpers\Features;
+use Hyde\Framework\Helpers\Meta;
+use Hyde\Framework\Models\Support\Author;
 
 return [
 
@@ -50,7 +50,7 @@ return [
         // Meta::name('twitter:creator', '@HydeFramework'),
         // Meta::name('description', 'My Hyde Blog'),
         // Meta::name('keywords', 'Static Sites, Blogs, Documentation'),
-        Meta::name('generator', 'HydePHP '.Hyde\Hyde::version()),
+        Meta::name('generator', 'HydePHP '.Hyde\Framework\Hyde::version()),
         Meta::property('site_name', config('site.name', 'HydePHP')),
     ],
 
@@ -100,9 +100,9 @@ return [
 
     'authors' => [
         Author::create(
-            'mr_hyde', // Required username
-            'Mr. Hyde', // Optional display name
-            'https://hydephp.com' // Optional website URL
+            username: 'mr_hyde', // Required username
+            name: 'Mr. Hyde', // Optional display name
+            website: 'https://hydephp.com' // Optional website URL
         ),
     ],
 
